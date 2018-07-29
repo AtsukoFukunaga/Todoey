7c2b907fd67ef7a309732da2f89628592ce61f94
AtsukoFukunaga <Atsuko.Fukunaga@hawaii.edu>
2018-07-29 12:04:03 -1000

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Atsuko Fukunaga on 7/29/18.
+//  Copyright © 2018 Atsuko Fukunaga. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+
+}
+
+
